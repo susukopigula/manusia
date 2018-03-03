e6eb4eff9bb48c7e4c0ea125fee2b1ba0dacc78e
susukopigula <susukopigula@gmail.com>
2018-03-03 17:24:02 +0700

    Class Manusia

---
+++ manusia/src/manusia/Manusia.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package manusia;
+
+/**
+ *
+ * @author pain
+ */
+public class Manusia {
+
+    /**
+     * @param args the command line arguments
+     */
+    String nama;
+    
+    public void tampilNama() {
+        System.out.println("Nama saya adalah "+ nama);
+    }
+}
---
+++ manusia/src/manusia/ManusiaBeraksi.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package manusia;
+
+/**
+ *
+ * @author pain
+ */
+public class ManusiaBeraksi {
+    public static void main(String[] args) {
+        Manusia yazid = new Manusia();
+        yazid.nama = "Muh Yazid";
+        
+        yazid.tampilNama();
+    }
+}
